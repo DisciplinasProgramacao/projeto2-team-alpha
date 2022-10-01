79c3a7b5ea4f08939f8cad0ae4a5e4a2df9963e0
Nikolas Louret <navlouret@sga.pucminas.br>
2022-10-01 17:17:39 -0300

    [Update] Finalização do método caminho euleriano #11

--- codigo/source/grafo/GrafoMutavel.java
+++ codigo/source/grafo/GrafoMutavel.java
@@ -1,28 +1,76 @@
 package source.grafo;
 
+import source.Arquivo;
+
 import java.io.IOException;
 
 public abstract class GrafoMutavel extends Grafo {
-    // #region Contrutor
+    //#region Contrutor
 
     public GrafoMutavel(String nome) {
         super(nome);
     }
-    // #endregion
+    //#endregion
 
-    // #region boolean Aresta
+    //#region boolean Vertice
+
+    public boolean delVertice(int idVertice) {
+        return delVertice(idVertice);
+    }
+    //#endregion
+
+    //#region boolean Aresta
 
     public abstract boolean addAresta(int origem, int destino);
 
-    public boolean delAresta(int origem, int destino) {
-        return false;
+    public boolean delAresta(int origem, int destino){
+        return delAresta(origem, destino);
     }
-    // #endregion
+    //#endregion
 
-    // #region Manipulação de Arquivo
+    //#region Manipular Arquivo
 
-    public abstract void carregar(String nomeArquivo) throws IOException;
+    public void carregar(String nomeArquivo) throws IOException {
+        Arquivo arq = new Arquivo("codigo/app/files/", nomeArquivo, "read");
 
-    public abstract void salvar(String nomeArquivo) throws Exception;
-    // #endregion
+        while (arq.ready()) {
+            String line = arq.readLine();
+            line = line.replaceAll("\\n", "");
+            String verticesPesos[] = line.split(";");
+            int verticeOrigem = Integer.parseInt(verticesPesos[0]);
+
+            for (String verticePeso : verticesPesos) {
+                int vertice = Integer.parseInt(verticePeso.split("-")[0]);
+                this.addVertice(vertice);
+
+                if (vertice != verticeOrigem) {
+                    int peso = Integer.parseInt(verticePeso.split("-")[1]);
+                    this.addAresta(verticeOrigem, vertice, peso);
+                }
+            }
+        }
+
+        arq.close();
+    }
+
+    public void salvar(String nomeArquivo) throws Exception {
+        Arquivo arq = new Arquivo("codigo/app/files/", nomeArquivo, "save");
+
+        for (Vertice vertice : this.getAllVertices()) {
+            arq.write(vertice.getId());
+
+            for (Aresta aresta : vertice.getAllArestas()) {
+                arq.write(";" + aresta.getDestino() + "-" + aresta.getPeso());
+            }
+
+            arq.write("\n");
+        }
+
+        arq.close();
+    }
+
+    public void salvar() throws Exception {
+        salvar(this.nome);
+    }
+    //#endregion
 }
