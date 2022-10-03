ecadc72d2348100fb588b725c864eb209734a698
Lucas-lutti <lucaspicinin@hotmail.com>
2022-10-03 08:08:16 -0300

    testsSubgrafo

--- codigo/source/Tests/TestGrafoCompleto.java
+++ codigo/source/Tests/TestGrafoCompleto.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import source.Lista;
 import source.grafo.GrafoCompleto;
+import source.grafo.GrafoNaoPonderado;
+import source.grafo.Vertice;
 
 public class TestGrafoCompleto {
 
@@ -50,12 +54,25 @@ public class TestGrafoCompleto {
 
     @Test
     void testSubGrafo() {
-        GrafoCompleto grafo1 = new GrafoCompleto("testaSubGrafo", 2);
-        grafo1.addVertice(1);
-        grafo1.addVertice(2);
-        grafo1.addVertice(3);
-        grafo1.addAresta(1,2,1);
-        grafo1.addAresta(2,3,1);
-        grafo1.addAresta(1,3,1);
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("grafoNaoPonderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.addVertice(4);
+        grafoNaoPonderado.addVertice(5);
+        grafoNaoPonderado.addAresta(1,2);
+        grafoNaoPonderado.addAresta(2,3);
+        grafoNaoPonderado.addAresta(1,3);
+        grafoNaoPonderado.addAresta(2,4);
+        grafoNaoPonderado.addAresta(1,4);
+        
+        Lista<Vertice> listaDeVertices = new Lista<Vertice>();
+        listaDeVertices.add(new Vertice(1));
+        listaDeVertices.add(new Vertice(2));
+        listaDeVertices.add(new Vertice(4));
+        GrafoNaoPonderado subGrafo = grafoNaoPonderado.subGrafo(listaDeVertices);
+        
+        assertEquals(4, subGrafo.ordem(),"Testa ordem do respectivo gravo");
+        
     }
 }
