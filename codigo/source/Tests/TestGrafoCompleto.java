2c1d11c9c4782de2468151d7ccd25efd1c1942fa
Lucas-lutti <lucaspicinin@hotmail.com>
2022-10-03 08:24:11 -0300

    Testes finalizando

--- codigo/source/Tests/TestGrafoCompleto.java
+++ codigo/source/Tests/TestGrafoCompleto.java
@@ -25,6 +25,7 @@ public class TestGrafoCompleto {
         assertTrue(grafo1.completo(),"Testa se o respectivo grafo é completo");
         
         
+        
     }
 
     @Test
@@ -51,28 +52,18 @@ public class TestGrafoCompleto {
         assertEquals(4, grafo1.ordem(),"Testa ordem do respectivo gravo");
     }
 
-
     @Test
-    void testSubGrafo() {
-        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("grafoNaoPonderado");
-        grafoNaoPonderado.addVertice(1);
-        grafoNaoPonderado.addVertice(2);
-        grafoNaoPonderado.addVertice(3);
-        grafoNaoPonderado.addVertice(4);
-        grafoNaoPonderado.addVertice(5);
-        grafoNaoPonderado.addAresta(1,2);
-        grafoNaoPonderado.addAresta(2,3);
-        grafoNaoPonderado.addAresta(1,3);
-        grafoNaoPonderado.addAresta(2,4);
-        grafoNaoPonderado.addAresta(1,4);
-        
-        Lista<Vertice> listaDeVertices = new Lista<Vertice>();
-        listaDeVertices.add(new Vertice(1));
-        listaDeVertices.add(new Vertice(2));
-        listaDeVertices.add(new Vertice(4));
-        GrafoNaoPonderado subGrafo = grafoNaoPonderado.subGrafo(listaDeVertices);
-        
-        assertEquals(3, subGrafo.ordem(),"Testa ordem do respectivo gravo");
-        
+    void testTamanho() {
+        GrafoCompleto grafo1 = new GrafoCompleto("testaGrafoCompleto", 2);
+        grafo1.addVertice(1);
+        grafo1.addVertice(2);
+        grafo1.addVertice(3);
+        grafo1.addAresta(1,2,1);
+        grafo1.addAresta(2,3,1);
+        grafo1.addAresta(1,3,1);
+        assertEquals(8,grafo1.tamanho(),"Testa tamanho do respectivo grafo");
     }
+
+
+    
 }
--- codigo/source/Tests/TestGrafoNaoPonderado.java
+++ codigo/source/Tests/TestGrafoNaoPonderado.java
@@ -1,10 +1,14 @@
 package source.Tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import source.Lista;
 import source.grafo.GrafoNaoPonderado;
+import source.grafo.Vertice;
 
 public class TestGrafoNaoPonderado {
 
@@ -23,11 +27,33 @@ public class TestGrafoNaoPonderado {
         grafoNaoPonderado.addAresta(5, 3);
         grafoNaoPonderado.addAresta(5, 4);
         grafoNaoPonderado.addAresta(4, 3);
-        
+        //Test de arestas (????)
+        assertEquals(5,grafoNaoPonderado.existeAresta(5,4),"Testa se aresta existe");
     }
+
+    
 
     @Test
     void testSubGrafo() {
-
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("grafoNaoPonderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.addVertice(4);
+        grafoNaoPonderado.addVertice(5);
+        grafoNaoPonderado.addAresta(1,2);
+        grafoNaoPonderado.addAresta(2,3);
+        grafoNaoPonderado.addAresta(1,3);
+        grafoNaoPonderado.addAresta(2,4);
+        grafoNaoPonderado.addAresta(1,4);
+        
+        Lista<Vertice> listaDeVertices = new Lista<Vertice>();
+        listaDeVertices.add(new Vertice(1));
+        listaDeVertices.add(new Vertice(2));
+        listaDeVertices.add(new Vertice(4));
+        GrafoNaoPonderado subGrafo = grafoNaoPonderado.subGrafo(listaDeVertices);
+        
+        assertEquals(3, subGrafo.ordem(),"Testa ordem do respectivo gravo");
+        
     }
 }
